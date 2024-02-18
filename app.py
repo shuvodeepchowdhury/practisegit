a6af0087ef2cbc3eb5339151cf4b40aca749dfdf
ShuvodeepChowdhury <shuvodeepchowdhury9365@gmail.com>
2024-02-18 17:27:08 +0530

    shuvodeep completed his coding

--- app.py
+++ app.py
@@ -6,3 +6,4 @@ def div (a,b):
     return a/b
 # checking ofr gitignore
 #again closed and checking for changes happened or not
+# i am shuvodeep i created a branch and i have written this using branch shuvodeep
